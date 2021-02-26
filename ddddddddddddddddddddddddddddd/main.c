139d9ca2a7ea1efa5abb2be816e498d30fcc6f24
soso45ahmed <sara.ahmed3811@gmail.com>
2021-02-26 15:25:15 +0200

    test

---
+++ ddddddddddddddddddddddddddddd/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int x  ;
+    printf("enter the number\n");
+    scanf("%d",&x);
+    int y = x/2;
+    if(y*2==x)
+    {
+        printf("number is even \n");
+    } else
+    {
+        printf("numer is odd \n");
+    }
+    /*int z , c , v ;
+    printf("enter the first number");
+    scanf("%d ,%d,%d",&z,&c,&v);
+    switch()
+    case
+
+    printf("Hello world!\n");*/
+    return 0;
+}
