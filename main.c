c076df4cc3194fd1b69d07fb6036fc6173412b71
soso45ahmed <sara.ahmed3811@gmail.com>
2021-02-26 15:38:28 +0200

    ret

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    //char letter= 'A';
+    //printf("%d\n",letter);
+    float x;
+    scanf("%f",&x);
+    printf("%g`\n",x);
+    printf("Hello world!\n");
+    return 0;
+}
